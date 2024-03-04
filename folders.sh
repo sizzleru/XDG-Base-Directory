74529fe3fb00551756ea602f9bfaf0d606fa4d33
sizzleru <github@sizzleru.com>
2024-03-05 05:58:26 +1100

    modified to add to path

--- folders.sh
+++ folders.sh
@@ -6,6 +6,4 @@ mkdir -pv "${XDG_BIN_HOME}"
 
 # Application compliance
 mkdir -pv "${XDG_CONFIG_HOME}/XDG"
-if [[ ! -f "${XDG_CONFIG_HOME}/XDG/packages.sh" ]]; then
-    touch "${XDG_CONFIG_HOME}/XDG/packages.sh"
-fi
+mkdir -pv "${XDG_BIN_HOME}/XDG"
