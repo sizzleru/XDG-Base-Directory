d549d3ab2289f23ebe823d59cfde8c92757729fd
sizzleru <github@sizzleru.com>
2024-03-05 06:18:54 +1100

    renamed n_flag to p_flag (and inverted)

--- packages/java.sh
+++ packages/java.sh
@@ -1,4 +1,4 @@
 if [[ -d "${HOME}/.java" ]]; then
-    mv -f "${HOME}/.java" "${XDG_CONFIG_HOME}/java"
+    mv -fv "${HOME}/.java" "${XDG_CONFIG_HOME}/java"
     "export _JAVA_OPTIONS=-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java" >> "${XDG_DATA_HOME}/XDG/packages.sh"
 fi
