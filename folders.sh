17c4c619a27c8fa3ef29b5d9b141eaeee2c682ab
sizzleru <github@sizzleru.com>
2024-03-04 11:27:45 +1100

    add application migration (to comply with XDG)

--- folders.sh
+++ folders.sh
@@ -3,3 +3,9 @@ mkdir -pv "${XDG_CACHE_HOME}"
 mkdir -pv "${XDG_DATA_HOME}"
 mkdir -pv "${XDG_STATE_HOME}"
 mkdir -pv "${XDG_BIN_HOME}"
+
+# Application compliance
+mkdir -pv "${XDG_CONFIG_HOME}/XDG"
+if [[ ! -f "${XDG_CONFIG_HOME}/XDG/packages.sh" ]]; then
+    touch "${XDG_CONFIG_HOME}/XDG/packages.sh"
+fi
